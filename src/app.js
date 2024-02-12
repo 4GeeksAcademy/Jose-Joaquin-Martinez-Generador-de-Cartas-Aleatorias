2e249856a5d1c9be70691bd1bbd31823a834061c
JoseJoaquinMartinez <61237112+JoseJoaquinMartinez@users.noreply.github.com>
2024-02-12 20:21:36 +0000

    changing few things and adding some functions

--- src/app.js
+++ src/app.js
@@ -1,11 +1,58 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const CARDNUMBERS = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+const SUITS = ["heart", "diamond", "club", "spade"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const getRandomNumber = () => {
+  let randomCardNumber =
+    CARDNUMBERS[Math.floor(Math.random() * CARDNUMBERS.length)];
+  return randomCardNumber;
 };
+
+const getRandomSuit = () => {
+  let randomSuit = SUITS[Math.floor(Math.random() * SUITS.length)];
+  return randomSuit;
+};
+
+const generateRandomCard = () => {
+  // getting the value Number ID and giving it the new number
+  let cardNumber = document.getElementById("cardValue");
+  cardNumber.innerHTML = getRandomNumber(CARDNUMBERS);
+
+  let suit = getRandomSuit(SUITS);
+
+  // Remove previously added classes
+  document.getElementById("topLeftSuit").classList.remove(...SUITS);
+  document.getElementById("bottomRightSuit").classList.remove(...SUITS);
+
+  // Add selected class
+  document.getElementById("topLeftSuit").classList.add(suit);
+  document.getElementById("bottomRightSuit").classList.add(suit);
+
+  //getting users inputs on height and width
+  let widthInput = document.getElementById("widthInput").value;
+  let heightInput = document.getElementById("heightInput").value;
+
+  //appliying the changes of widht and height
+  document.getElementById("randomCard").style.width = widthInput + "px";
+  document.getElementById("randomCard").style.height = heightInput + "px";
+};
+
+window.onload = generateRandomCard;
+
+// adding a button to allow the user to pick random cards
+let btnRandomCardPicker = document.getElementById("btnRandomCard");
+
+btnRandomCardPicker.addEventListener("click", generateRandomCard);
+
+// Call randomCardGenerator every 6 seconds
+setInterval(generateRandomCard, 6000);
+
+//button to reset back to normal setting
+let btnClearChangesSize = document.getElementById("btnClear");
+btnClearChangesSize.addEventListener("click", function() {
+  document.getElementById("widthInput").value = "";
+  document.getElementById("heightInput").value = "";
+  document.getElementById("randomCard").style.width = "35%";
+  document.getElementById("randomCard").style.height = "60%";
+});
